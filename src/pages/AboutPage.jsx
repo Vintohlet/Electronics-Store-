3e9d9e3055049e8bfcc8857835e597689032640f
Ruslan Tuleuov <ruslantuleuoff@gmail.com>
2025-02-11 21:39:59 +0500

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,12 @@
 import { Box } from "@mui/joy";
 import { Container, Typography } from "@mui/material";
-
+import aboutImage from "../assets/img/bouts.jpg";
 export default function AboutPage(){
     return(
         <Container sx={{display:"flex", justifyContent:"center", alignItems:"center", width:"100%", height:"100vh"}}>
         <Box sx={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
             <Typography variant="h3" sx={{mb:"24px"}}>This website was created for an educational project and is not a public offer. The rights to the product images belong to Halyk Bank. The creators of the website in telegram @maximohmy, @velocerr</Typography>
-            <img src="./src/assets/img/bouts.jpg" alt="about us" />
+            <img src={aboutImage} alt="about us" />
         </Box>
         </Container>
     )
